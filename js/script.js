c4cc0d8c506f53c778f675f5d674d79d862b9d96
Przemek Kawecki Toshiba <przemek.kawecki@gmail.com>
2021-06-16 01:22:41 +0200

    Add 'add active class functionality

--- js/script.js
+++ js/script.js
@@ -6,19 +6,24 @@
 
     const titleClickHandler = function(event){
 
+        event.preventDefault();
         console.log(event);
         console.log('Link was clicked');
         
-        /* remove class 'active' from all article links  */
+        /*[DONE] remove class 'active' from all article links  */
         const activeLinks = document.querySelectorAll('.titles a.active');
         for (let activeLink of activeLinks) {
             activeLink.classList.remove('active');
         }
 
-        /* add class 'active' to the clicked link */
+        /* [IN PROGRESS] add class 'active' to the clicked link */
+        const clickedElement = this;
+        console.log('clickedElement:',clickedElement);
+        console.log('clickedElement(with plus):' + clickedElement);
+        clickedElement.classList.add('active');
 
 
-        /* remove class 'active' from all articles */
+        /* [DONE] remove class 'active' from all articles */
         const aciveArticles = document.querySelectorAll(".posts .active");
         for(let activeArticle of aciveArticles) {
             activeArticle.classList.remove('active');
@@ -26,10 +31,15 @@
 
 
         /* get 'href' attribute from the clicked link */
+        let hrefAtt = clickedElement.getAttribute('href');
+        console.log(hrefAtt);
 
         /* find the correct article using the selector (value of 'href' attribute) */
-
+        let searchedArticle = document.querySelector(hrefAtt);
+        console.log(searchedArticle);
+        
         /* add class 'active' to the correct article */
+        searchedArticle.classList.add('active');
     }
 
     const links = document.querySelectorAll('.titles a');
